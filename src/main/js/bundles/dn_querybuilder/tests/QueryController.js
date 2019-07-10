b896f68318aa2f2d048037677712d8abd59d5cea
m.stein <m.stein@conterra.de>
2019-07-10 16:12:11 +0200

    Add first test

---
+++ src/main/js/bundles/dn_querybuilder/tests/QueryController.js
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2019 con terra GmbH (info@conterra.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import registerSuite from "intern!object";
+import assert from "intern/chai!assert";
+import module from "module";
+import QueryController from "../QueryController";
+import Replacer from "dn_queryplaceholder/Replacer";
+
+registerSuite({
+    name: module.id,
+    "SearchReplacer": function () {
+        const object = {"$or": [{"aeroway": {"$eq": "${current_app_name}"}}]};
+        const queryController = new QueryController();
+        const replacer = new Replacer();
+        const namePlaceholderProcvider = {
+            getPlaceholder() {
+                const placeholder = {};
+                const appName = "Peter";
+                const appTitle = "Peters App";
+                placeholder["current_app_name"] = appName;
+                placeholder["current_app_title"] = appTitle;
+                return placeholder;
+            }
+        };
+        replacer.addPlaceholderProvider(namePlaceholderProcvider);
+        queryController._replacer = replacer;
+        queryController.searchReplacer(object);
+        assert.equal(object["$or"][0].aeroway["$eq"], "Peter", 'the replacement failed.');
+    }
+});
